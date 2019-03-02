8d1645ecbf6c00b0fb3f1b4bcf870300a7d7f69b
Angela Crepaldi <angelacrepaldi@gmail.com>
2019-03-02 00:32:54 -0300

    Created main.py

---
+++ main.py
@@ -0,0 +1,26 @@
+# Types
+print("Aloha!")
+print(5+6)
+print("5"+"6")
+print(2.73*3.14)
+print(int("5")+int("7"))
+print('She said: "I want that."')
+print('She said: "Don\'t do that."')
+print("This costs " + str(6+5) + " dollars.")
+print("My name is " + "Hello:Angela:Nick".split(":")[1])
+
+# Boolean operations
+print(5 == 5)
+print(5 == 4)
+print(5 is not 4)
+print(5 is 4)
+print("This" is "This")
+print("True" is True)
+print("True" is str(True))
+
+# Lists (arrays)
+
+print("I like " + ["barbecue", "movies", "pizza"][1])
+
+# Dictionaries
+print({"name": "Angela", "age": 32, "hobby": "code"}["name"])
