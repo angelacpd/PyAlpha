0e98ba3a1bc39da7d06ac34576faf0c17a4be3d8
Angela Crepaldi <angelacrepaldi@gmail.com>
2019-10-29 04:44:14 -0300

    Using json package.

---
+++ hiJson.py
@@ -0,0 +1,18 @@
+import simplejson as json  # rename package
+# download and install simplejson pip3 install simplejson
+import os  # import os to handle files
+
+# Check if there is a file and if the file is not empty
+if os.path.isfile("./ages.json") and os.stat("./ages.json").st_size != 0:
+    old_file = open("./ages.json", "r+")
+    data = json.loads(old_file.read())
+    print("Current age is:", data["age"], "adding a year.")
+    data["age"] = data["age"] + 1
+    print("New age is ", data["age"])
+else:
+    old_file = open("./ages.json", "w+")
+    data = {"name": "Angie", "age": 33}
+    print("No file found, setting default age to ", data["age"])
+
+old_file.seek(0)
+old_file.write(json.dumps(data))
